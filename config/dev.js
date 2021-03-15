aa42add8e132aa564cc77129a207c401a0288bbf
Kimchi Developer <65699391+Kimchi-dev@users.noreply.github.com>
2021-03-15 16:19:12 +0900

    dsa
    d

---
+++ config/dev.js
@@ -0,0 +1,3 @@
+module.exports = {
+    mongoURI:'mongodb+srv://kimchi-dev:shwm250s4138@cluster0.etkbx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+}
--- index.js
+++ index.js
@@ -2,9 +2,20 @@
 const express = require('express')
 const app = express()
 const port = 5000
+const bodyParser = require('body-parser');
+
+const config = require('./config/key')
+
+const { User } = require("./models/User");
+
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended: true})) ;
+
+app.use(bodyParser.json());
 
 const mongoose = require('mongoose')
-mongoose.connect('mongodb+srv://kimchi-dev:shwm250s4138@cluster0.etkbx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority',{
+mongoose.connect(config.mongoURI,{
     useNewUrlParser:true,
     useUnifiedTopology: true,
     useCreateIndex: true,
@@ -15,9 +26,20 @@ mongoose.connect('mongodb+srv://kimchi-dev:shwm250s4138@cluster0.etkbx.mongodb.n
 
 
 app.get('/', (req, res) => {
-    res.send('Hello World!')
+    res.send('Hello World dd!')
+})
+
+app.post('/register',(req,res) => {
+    const user = new User(req.body)
+
+    user.save((err,userInfo) => {
+       if(err) return res.json({success: false, err})
+        return res.status(200).json({
+            success: true
+        })
+    })
 })
 
 app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`)
+    console.log('Example app listening at http://localhost:${port}')
 })
