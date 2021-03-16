8baf898290ed68d7524ee2f39120dd784b80ab73
Kimchi Developer <65699391+Kimchi-dev@users.noreply.github.com>
2021-03-16 17:57:58 +0900

    login function

--- index.js
+++ index.js
@@ -2,18 +2,18 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const bodyParser = require('body-parser');
-
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
 const config = require('./config/key')
 
-const { User } = require("./models/User");
+const { User } = require("./models/User")
 
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended: true})) ;
 
 app.use(bodyParser.json());
-
+app.use(cookieParser());
 const mongoose = require('mongoose')
 mongoose.connect(config.mongoURI,{
     useNewUrlParser:true,
@@ -40,6 +40,33 @@ app.post('/register',(req,res) => {
     })
 })
 
+app.post('/login',(req,res) =>{
+    //요청된 이메일을 데이터베이스에서 중복확인
+    User.findOne({email: req.body.email },(err,user) => {
+        if(!user) {
+            return res.json({
+                loginSuccess: false,
+                message: "없는 이메일입니다."
+            })
+        }
+        //비밀번호 확인
+        user.comparePassword(req.body.password,(err,isMatch) => {
+            if(!isMatch)
+                return res.json({loginSuccess: false, message:"비밀번호가 틀림"})
+        })
+        //비밀번호 맞다면 토큰생성
+        user.generateToken((err, user) =>{
+            if(err) return res.status(400).send(err)
+
+            res.cookie("x_auth",user.token)
+                .status(200)
+                .json({loginSuccess: true, userId: user._id})
+
+        })
+    })
+
+
+})
 app.listen(port, () => {
-    console.log('Example app listening at http://localhost:${port}')
+    console.log('Example app listening at http://localhost:'+port)
 })
--- models/User.js
+++ models/User.js
@@ -1,4 +1,8 @@
 const mongoose = require('mongoose')
+const bcrypt = require('bcrypt')
+const saltRounds = 10;
+const jwt = require('jsonwebtoken')
+
 
 const userSchema = mongoose.Schema({
     name: {
@@ -27,6 +31,45 @@ const userSchema = mongoose.Schema({
     }
 
 })
+
+userSchema.pre('save',function(next){
+    var user = this
+    //비밀번호 암호화
+    if(user.isModified('password')){
+
+        bcrypt.genSalt(saltRounds,function(err, salt){
+            if(err) return next(err)
+
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash
+                next()
+            })
+        })
+    }else{
+        next()
+    }
+
+})
+
+userSchema.methods.comparePassword = function(plainPassword,callback){
+    bcrypt.compare(plainPassword, this.password, function(err, isMatch){
+        if(err) return callback(err)
+            callback(null,isMatch)
+    })
+}
+
+userSchema.methods.generateToken = function(callback){
+    var user = this;
+
+    // jsonwebtoken을 이용해서 생성하기
+    var token = jwt.sign(user._id.toHexString(),'secretToken')
+    user.token = token
+    user.save(function(err,user){
+        if(err) return callback(err)
+        callback(null,user)
+    })
+}
 
 const User = mongoose.model('User',userSchema)
 
